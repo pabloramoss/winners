7a3673462504cf57c0831839854ec44d5e75e224
pabloramoss <pablo-362@hotmail.com>
2022-10-03 23:26:22 -0300

    add accordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,69 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Stack, Text } from '@chakra-ui/react'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const premios = [
+    {
+      nombreTerna: "Mejor streamer de LOL",
+      nominados: [
+        {nombre: "josedeodo"},
+        {nombre: "grafo"},
+        {nombre: "shelao"},
+        {nombre: "tellier"},
+      ],
+      ganadores: ["grafo", "shelao"]
+    },
+    {
+      nombreTerna: "Mejor streamer de fortnite",
+      nominados: [
+        {nombre: "zeko"},
+        {nombre: "king"},
+        {nombre: "escaracho"},
+        {nombre: "santidead"},
+      ],
+      ganadores: ["king", "zeko"]
+    }
+  ]
+
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Stack>
+      <Accordion allowMultiple>
+        {premios.map((item: any) =>         
+        <AccordionItem key={item.nombreTerna}>
+          <h2>
+            <AccordionButton>
+              <Box flex='1' textAlign='left'>
+                {item.nombreTerna}
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            <ul>{item.nominados.map((item: any) => <li key={item.nombre}>{item.nombre}</li>)}</ul>
+            <Accordion allowMultiple>
+              {item.ganadores.map((ganador: any, index: number) =>         
+              <AccordionItem key={index}>
+                <h2>
+                  <AccordionButton>
+                    <Box flex='1' textAlign='left'>
+                      {`Ganador ${index + 1}`}
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                  <Text>{ganador}</Text>
+                </AccordionPanel>
+              </AccordionItem>)}
+            </Accordion>
+          </AccordionPanel>
+        </AccordionItem>)}
+      </Accordion>
+    </Stack>
   )
 }
 
